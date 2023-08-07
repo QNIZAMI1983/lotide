27a82a0d359f42032f2a7409034004232d62ccf9
Qais Nizami <qais.nizami@gmail.com>
2023-08-07 05:07:04 -0400

    committing dried middle.js and middleTest.js

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// Test cases
+assertArraysEqual(middle([1]), []); // Should pass
+assertArraysEqual(middle([1, 2]), []); // Should pass
+assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Should pass
