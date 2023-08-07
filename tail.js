4bf8a7bb811d3c85adf822560e544d608181a9ba
Qais Nizami <qais.nizami@gmail.com>
2023-08-07 04:01:11 -0400

    committing dry tail.js and relevant tests

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-// Function Assert Equal
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require('./assertEqual');
 
   // Tail Function
 const tail = function(array) {
@@ -16,12 +9,5 @@ const tail = function(array) {
     }
   };
   
-  // TEST CASES
-  const testArray1 = ["Hello", "Lighthouse", "Labs"];
-  const result1 = tail(testArray1);
-  console.log(result1); // prints ["Lighthouse", "Labs"]
-  
-  const testArray2 = [1, 2, 3];
-  const result2 = tail(testArray2);
-  console.log(result2); // prints [2, 3]
+  module.exports = tail;
   
