e75fadb0c15dfaedf91c5c206ccd1e582032d96d
Qais Nizami <qais.nizami@gmail.com>
2023-08-07 05:41:00 -0400

    installing Mocha & Chai

--- head.js
+++ head.js
@@ -1,12 +1,12 @@
-const assertEqual = require('./assertEqual');
+// const assertEqual = require('./assertEqual');
 
- // Function for Head
-const head = function(array) {
-    if (array.length === 0) {
-      return undefined; // Empty array should yield undefined as its head
-    } else {
-      return array[0]; // Return the first item of the array
-    }
-  };
-  
-  module.exports = head;
+// Function for Head
+const head = function (array) {
+  if (array.length === 0) {
+    return undefined; // Empty array should yield undefined as its head
+  } else {
+    return array[0]; // Return the first item of the array
+  }
+};
+
+module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,24 @@
+const assert = require('chai').assert;
 const head = require('../head');
 
- // TEST CASES
- const testArray1 = [5, 6, 7];
- assertEqual(head(testArray1), 5); // The first item of [5, 6, 7] is 5
- 
- const testArray2 = ["Hello", "Lighthouse", "Labs"];
- assertEqual(head(testArray2), "Hello"); // The first item of ["Hello", "Lighthouse", "Labs"] is "Hello"
- 
- const testArray3 = [42];
- assertEqual(head(testArray3), 42); // The first item of [42] is 42
- 
- const testArray4 = [];
- assertEqual(head(testArray4), undefined); // An empty array should yield undefined as its head
- 
+describe("#head", () => {
+    it("returns 5 for [5, 6, 7]", () => {
+        const testArray1 = [5, 6, 7];
+        assert.strictEqual(head(testArray1), 5);
+    });
+
+    it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+        const testArray2 = ["Hello", "Lighthouse", "Labs"];
+        assert.strictEqual(head(testArray2), "Hello");
+    });
+
+    it("returns 42 for [42]", () => {
+        const testArray3 = [42];
+        assert.strictEqual(head(testArray3), 42);
+    });
+
+    it("returns undefined for an empty array", () => {
+        const testArray4 = [];
+        assert.strictEqual(head(testArray4), undefined);
+    });
+});
